85d1b8bcd5cd8ddd9543b8a39abe1629f24e2109
Swen Mulderij <swenmulderij@gmail.com>
2017-05-19 14:45:50 +0200

    Nieuwe componenten

--- kwhm-code/kwhm-code.ino
+++ kwhm-code/kwhm-code.ino
@@ -1,6 +1,10 @@
+#include <Ultrasonic.h>
+
 #include <Key.h>
 #include <Keypad.h>
 #include <ctype.h>
+#include <Servo.h>
+#include "pitches.h"
 
 /////// Begin Kunnen wij het Maken?! code ////////
 
@@ -12,11 +16,80 @@ char keys[4][4] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
+
 byte rowPins[4] = {2, 3, 4, 5};
 byte colPins[4] = {6, 7, 8, 9};
+
+byte servopin = 9;
+byte transistorpin = 13;
+byte speakerpin = 11;
+byte sensorpin = 0;
+byte trigpin = 12;
+byte echopin = 13;
+byte knoppin = 10;
+
 Keypad toetsenbord = Keypad( makeKeymap(keys), rowPins, colPins, 4, 4);
+Servo mijnservo;
 
 /// Einde instellingen componenten ///
+void wacht_op_knop(int knoppin = knoppin) {
+    pinMode(knoppin, INPUT);
+    while (true) {
+    int buttonState = digitalRead(10);
+    if (buttonState == HIGH) {
+      slaap(0.3);
+      return;
+    }
+    slaap(0.1);
+  }
+}
+
+
+int cm_afstand(int trigpin = trigpin, int echopin = echopin) {
+  Ultrasonic uss = Ultrasonic(trigpin, echopin);
+  bool distancefound = false;
+  int distance = 0;
+  while (not distancefound) {
+    int distance1 = uss.distanceRead();
+    slaap(0.1);
+    int distance2 = uss.distanceRead();
+
+    if (abs(distance1 - distance2) < 5) {
+      distance = ((distance1 + distance2)/2);
+      distancefound = true;
+    }
+  }
+  if (distance > 50) {
+    return 100;
+  }
+  else {
+    return map(uss.distanceRead(), 0, 50, 0, 100);
+  }  
+}
+
+int lees_sensor(int sensorpin = sensorpin) {
+  return map(analogRead(sensorpin), 0, 1024, 0 ,100);
+}
+
+void speel_toon(int toon, float seconde, int pinummer = speakerpin) {
+  tone(pinummer, toon);
+  slaap(seconde);
+  noTone(pinummer);
+}
+
+void dcmotor_seconde(float seconde, int pinummer = transistorpin) {
+  pinMode(transistorpin, OUTPUT);
+  digitalWrite(transistorpin, HIGH);
+  slaap(seconde);
+  digitalWrite(transistorpin, LOW);
+}
+
+void positioneer_servo(int hoek, int pinnummer = servopin) {
+  mijnservo.attach(servopin);
+  mijnservo.write(hoek);
+  delay(30);
+//  mijnservo.detach();
+}
 
 void toon_op_scherm(String tekst) {
   Serial.println(tekst);
@@ -82,10 +155,14 @@ void slaap(float seconde) {
 
 
 /////// Begin eigen code ////////
-  void setup() {
-    // Dit wordt eenmaal uitgevoerd
-  }
-  void loop() {
-    // Dit wordt constant herhaald
-  }
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  wacht_op_knop();
+  Serial.println("hai");
+
+}
 /////// Einde eigen code ////////
